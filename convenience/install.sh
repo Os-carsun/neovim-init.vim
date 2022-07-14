34c32c2688dd80a6dd0e67836d4b739d81fda0b1
oscar.sun <good0991@gmail.com>
2022-07-14 12:42:38 +0800

    fix: install issues

--- convenience/install.sh
+++ convenience/install.sh
@@ -8,6 +8,7 @@
 NEOVIM_VERSION=0.7.0
 NVM_VERSION=0.39.1
 NODE_VERSION=18.0.0
+LUA_PATH=/usr/local/share/lua/5.1
 
 # Check if this script is being run in the "convenience" directory
 if ! [[ "$PWD" = */convenience ]]; then
@@ -77,14 +78,12 @@ if ! [[ ":$PATH:" == *":$HOME/.local/bin:"* ]]; then
 fi
 
 # Install nvm, node, npm, language servers
-echo "[*] Installing nvm $NVM_VERSION ..."
-curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh" | bash # installs Node Version Manager to ~/.nvm, it also detects bash or zsh and appends source lines to ~/.bashrc or ~/.zshrc accordingly
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # to get the nvm command working without sourcing bash configs
+echo "[*] Installing n ..."
+brew install n
+export N_PREFIX=$HOME/.n
+export PATH=$N_PREFIX/bin:$PATH
 echo "[*] Installing node $NODE_VERSION ..."
-nvm install $NODE_VERSION # installs specific version of Node
-echo "[*] Setting npm config to use ~/.local as prefix ..."
-npm config set prefix '~/.local/' # npm install -g will now install to ~/.local/bin
+n $NODE_VERSION # installs specific version of Node
 echo "[*] Installing language servers ..."
 npm i -g pyright # python language server
 #npm i -g typescript typescript-language-server # uncomment to install typescript language server. remember to add "tsserver" to the "servers" list in init.vim within the lua section. See https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for other language servers.
@@ -93,6 +92,7 @@ npm i -g pyright # python language server
 echo '[*] Installing Python dependencies in a virtual environment ...'
 python3 -m venv ~/.config/nvim/env
 source ~/.config/nvim/env/bin/activate
+pip install --upgrade pip
 pip install wheel
 pip install pynvim doq
 deactivate
@@ -108,6 +108,8 @@ nvim -c 'PlugInstall' -c 'qa'
 
 # Copy init.vim and lua scripts in current working directory to nvim's config location
 echo '[*] Copying init.vim & lua/ -> ~/.config/nvim/'
-cp -r ../init.vim ../lua/ ~/.config/nvim/
+mkdir -p "$LUA_PATH" 
+cp -r ../init.vim ~/.config/nvim/
+cp -r ../lua/ "$LUA_PATH"
 
 echo -e "[+] Done, welcome to your new \033[1m\033[92mneovim\033[0m experience! Try it by running: nvim. (NOTE, remember to: source $SHELL_CONFIG_FILE) Want to customize it? Modify ~/.config/nvim/init.vim! Remember to change your terminal font to a nerd font :)"
